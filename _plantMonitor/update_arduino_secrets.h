7f2b88d14019f906d720fba457e0bd2e60e679e0
elinor-oren <127933946+elinor-oren@users.noreply.github.com>
2023-11-01 08:50:18 +0000

    Create update_arduino_secrets.h
    Template for users to update their Wifi and MQTT connectivity

---
+++ _plantMonitor/update_arduino_secrets.h
@@ -0,0 +1,7 @@
+//Update the following data with your connectivity information; it is called as arduino_secrets.h
+#define SECRET_SSID "your ssid";
+#define SECRET_PASS "your WiFi password";
+#define MQTT_SERVER "your MQTT broker address";
+#define MQTT_PORT the port of your broker;
+#define SECRET_MQTTUSER "your MQTT user";
+#define SECRET_MQTTPASS "your mqtt password";
