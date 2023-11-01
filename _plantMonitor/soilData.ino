2348a9a1ec6f68f53b81c6b7ea4d8235e9982e76
elinor-oren <127933946+elinor-oren@users.noreply.github.com>
2023-11-01 10:42:04 +0000

    Add files via upload
    This is the commented file.

---
+++ _plantMonitor/soilData.ino
@@ -0,0 +1,268 @@
+
+//Libraries to include:
+#include <ESP8266WiFi.h> //turns on wifi
+#include <ESP8266WebServer.h> //runs the web server?
+#include <ezTime.h> //provides local + UTC time
+#include <PubSubClient.h> //provides a client for doing simple publish/subscribe messaging 
+
+//Libraries for DHT Sensor: 
+#include <DHT.h>
+#include <DHT_U.h>
+
+//Define sensor type, can include 12 or 11 
+#define DHTTYPE DHT22   
+
+// Sensors - DHT22 and Nails
+uint8_t DHTPin = 12;        // set the DHC sensor 
+uint8_t soilPin = 0;      // set the voltage reading 
+//
+float Temperature;        
+float Humidity;
+int Moisture = 1; // initial value just in case web page is loaded before readMoisture called
+int sensorVCC = 13;
+int blueLED = 2;
+DHT dht(DHTPin, DHTTYPE);   // Initialize DHT sensor.
+
+
+// Create a secret library / tab with your Wifi and MQTT login information 
+#include "arduino_secrets.h" 
+/*
+
+* use
+
+#define SECRET_SSID "ssid name"
+#define SECRET_PASS "ssid password"
+#define SECRET_MQTTUSER "user name - eg student"
+#define SECRET_MQTTPASS "password";
+ */
+
+//this bit actually connects to the wifi
+const char* ssid     = SECRET_SSID;
+const char* password = SECRET_PASS;
+const char* mqttuser = SECRET_MQTTUSER;
+const char* mqttpass = SECRET_MQTTPASS;
+
+//PURPOSE: Initialize web server 
+ESP8266WebServer server(80);
+const char* mqtt_server = "mqtt.cetools.org"; //update with your MQTT server 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// Update with your appropriate date and time
+Timezone GB;
+
+
+//PURPOSE: Start the sensor, wifi, MQTT publishing  
+void setup() { 
+  // Set up LED to be controllable via broker
+  // Initialize the BUILTIN_LED pin as an output
+  // Turn the LED off by making the voltage HIGH
+  pinMode(BUILTIN_LED, OUTPUT);     
+  digitalWrite(BUILTIN_LED, HIGH);  
+
+  // Set up the outputs to control the soil sensor
+  // switch and the blue LED for status indicator
+  pinMode(sensorVCC, OUTPUT); 
+  digitalWrite(sensorVCC, LOW);
+  pinMode(blueLED, OUTPUT); 
+  digitalWrite(blueLED, HIGH);
+
+  //PURPOSE: The serial connection is used for debugging purposes.
+  Serial.begin(115200); //This line initializes a serial communication port at a baud rate of 115200
+  delay(100);
+
+  // start DHT sensor
+  pinMode(DHTPin, INPUT);
+  dht.begin();
+
+  //run initialization functions
+  startWifi();
+  startWebserver();
+  syncDate();
+
+  //start MQTT server
+  client.setServer(mqtt_server, 1884);
+  client.setCallback(callback);
+
+}
+//PURPOSE: handles webserver requests, checks minutes, reads moisture data, sends data via MQTT, prints the current time to the serial monitor
+void loop() {
+  //PURPOSE: check for and handle incoming client requests
+  server.handleClient(); 
+
+  if (minuteChanged()) {
+    readMoisture();
+    sendMQTT();
+    Serial.println(GB.dateTime("H:i:s")); // UTC.dateTime("l, d-M-y H:i:s.v T")
+  }
+  
+  client.loop();
+}
+//****** FUNCTIONS BELOW THIS LINE *******
+
+//PURPOSE: printing the moisture value (previously read from the sensor) to the serial monitor
+void readMoisture(){
+  
+  // power the sensor
+  digitalWrite(sensorVCC, HIGH);
+  digitalWrite(blueLED, LOW);
+  delay(100);
+  // read the value from the sensor:
+  Moisture = analogRead(soilPin);   // PURPOSE: function reads the voltage on the specified pin and converts it into a digital value      
+  digitalWrite(sensorVCC, LOW);  //PURPOSE: conserves power, prevents electrolysis if the sensor is left on continuously
+  digitalWrite(blueLED, HIGH);
+  delay(100);
+  Serial.print("Wet ");
+  Serial.println(Moisture);   // PURPOSE: read the value from the nails
+}
+
+void startWifi() {
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // check to see if connected and wait until you are
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void syncDate() {
+  // get real date and time; displays the time in both UTC and the London time zone
+  waitForSync();
+  Serial.println("UTC: " + UTC.dateTime()); //PURPOSE: ensures that Arduino clock is synchronized with (UTC)
+  GB.setLocation("Europe/London");
+  Serial.println("London time: " + GB.dateTime());
+
+}
+
+void sendMQTT() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  snprintf (msg, 50, "%.1f", Temperature);
+  Serial.print("Publish message for t: ");
+  Serial.println(msg);
+  client.publish("student/CASA0014/plant/ucbvren/temperature", msg);
+
+  Humidity = dht.readHumidity(); // Gets the values of the humidity
+  snprintf (msg, 50, "%.0f", Humidity);
+  Serial.print("Publish message for h: ");
+  Serial.println(msg);
+  client.publish("student/CASA0014/plant/ucbvren/humidity", msg);
+
+  //Moisture = analogRead(soilPin);   // moisture read by readMoisture function
+  snprintf (msg, 50, "%.0i", Moisture);
+  Serial.print("Publish message for m: ");
+  Serial.println(msg);
+  client.publish("student/CASA0014/plant/ucbvren/moisture", msg);
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    
+    // Attempt to connect with clientID, username and password
+    if (client.connect(clientId.c_str(), mqttuser, mqttpass)) {
+      Serial.println("connected");
+      // ... and resubscribe
+      client.subscribe("student/CASA0014/plant/ucbvren/inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void startWebserver() {
+  // when connected and IP address obtained start HTTP server  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  Serial.println("HTTP server started");  
+}
+
+void handle_OnConnect() {
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity
+  server.send(200, "text/html", SendHTML(Temperature, Humidity, Moisture));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float Temperaturestat, float Humiditystat, int Moisturestat) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>ESP8266 DHT22 Report</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>ESP8266 Huzzah DHT22 Report</h1>\n";
+
+  ptr += "<p>Temperature: ";
+  ptr += (int)Temperaturestat;
+  ptr += " C</p>";
+  ptr += "<p>Humidity: ";
+  ptr += (int)Humiditystat;
+  ptr += "%</p>";
+  ptr += "<p>Moisture: ";
+  ptr += Moisturestat;
+  ptr += "</p>";
+  ptr += "<p>Sampled on: ";
+  ptr += GB.dateTime("l,");
+  ptr += "<br>";
+  ptr += GB.dateTime("d-M-y H:i:s T");
+  ptr += "</p>";
+
+  ptr += "</div>\n";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
+}
